9bfb986105b8e632d9a17263344942c3c9025da8
Aayush Gupta <guptaaayush432@gmail.com>
2022-12-16 20:12:37 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
+/* hardhat.config.js */
 require("@nomicfoundation/hardhat-toolbox");
+// require("dotenv").config({ path: ".env" });
+// require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+// const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    //  unused configuration commented out for now
+    // mumbai: {
+    //   url: ALCHEMY_API_KEY_URL,
+    //   accounts: [PRIVATE_KEY],
+    // },
+  },
+  solidity: {
+    version: "0.8.4",
+  },
+  // etherscan: {
+  //   apiKey: {
+  //     polygonMumbai: POLYGONSCAN_KEY,
+  //   },
+  // },
 };
